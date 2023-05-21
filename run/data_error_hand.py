1eb5f3b702617eb046441a4ef910dc47f4674782
learn <learn_lian@juzhenpei.com>
2023-05-21 11:07:14 +0800

    错误数据处理

---
+++ run/data_error_hand.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import os
+import numpy as np
+import sys
+
+path = str(sys.path[0]).replace('\\run','')
+camp = pd.read_csv(path + '/data/teleco_camp_orig.csv')
+# 脏数据或数据不正确
+import matplotlib.pyplot as plt
+
+plt.hist(camp['AvgIncome'], bins=20, normed=True)
+# 查看人均收入分布情况
+# plt.show()
+camp['AvgIncome'].describe(include='all')
+
+plt.hist(camp['AvgHomeValue'], bins=20, normed=True)
+camp['AvgHomeValue'].describe(include='all')
+plt.show()
+# 这里的0值应该是缺失值，处理0值
+camp['AvgIncome'] = camp['AvgIncome'].replace({0: np.NaN})
+# print(camp.AvgIncome.min())
+# print(camp.AvgIncome.max())
+# 由于数据中存在缺失值，因此需要指定绘图的值域
+plt.hist(camp['AvgIncome'], bins=20, normed=True, range=(camp.AvgIncome.min(), camp.AvgIncome.max()))
+# 处理0值后的AvgHomeValue
+# plt.show()
+
+camp['AvgIncome'].describe(include='all')
+
+camp['AvgHomeValue'] = camp['AvgHomeValue'].replace({0: np.NaN})
+
+plt.hist(camp['AvgHomeValue'], bins=20, normed=True, range=(camp.AvgHomeValue.min(), camp.AvgHomeValue.max()))
+
+camp['AvgHomeValue'].describe(include='all')
+
+
+
+
+
+
+
+
